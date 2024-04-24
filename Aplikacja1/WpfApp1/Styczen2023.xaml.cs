17d98810c0ebddb4b6de783c3d7935098ec8fe18
ForSureNotReaper <kobra.olinek13@gmail.com>
2024-04-24 14:10:11 +0200

    styczcen2023 gotowy

--- Aplikacja1/WpfApp1/Styczen2023.xaml.cs
+++ Aplikacja1/WpfApp1/Styczen2023.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,6 +31,43 @@ namespace WpfApp1
             MainWindow.Show();
 
             this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double cena;
+            if(pocztowka.IsChecked == true)
+            {
+                cena = 1;
+            }
+            else if(list.IsChecked == true)
+            {
+                cena = 1.5;
+            }
+            else if(paczka.IsChecked == true)
+            {
+                cena = 10;
+            }
+            else
+            {
+                cena= 0;
+            }
+            cena_txt.Text = "Cena: " + cena + "zł";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string kod_poczt = kod.Text;
+            string adres = ulica.Text;
+            string miastot = miasto.Text;
+            if(kod_poczt.Length==6 && kod_poczt[2] == '-')
+            {
+                MessageBox.Show("Przesyłka zostanie dostarczona na adres " + miastot+" " + kod_poczt + " na adres " + adres);
+            }
+            else
+            {
+                MessageBox.Show("Kod pocztowy jest nieprawidłowy");
+            }
         }
     }
 }
